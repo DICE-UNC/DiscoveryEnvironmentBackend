3ac46d69cfc0b03182a0502df4129f28f9df6b78
michael-conway <michael_conway@unc.edu>
2015-09-25 16:06:13 -0400

    #2 testing of env props pickup for donkey

--- libs/clj-jargon/project.clj
+++ libs/clj-jargon/project.clj
@@ -21,5 +21,7 @@
                  [org.iplantc/clojure-commons "5.0.0"]]
   :repositories [["dice.repository"
                   {:url "https://raw.github.com/DICE-UNC/DICE-Maven/master/releases"}]
+                 ["dice..snapshot.repository"
+                  {:url "https://raw.github.com/DICE-UNC/DICE-Maven/master/snapshots"}]
                  ["iplant.repository"
                   {:url "https://everdene.iplantcollaborative.org/archiva/repository/internal/"}]])
